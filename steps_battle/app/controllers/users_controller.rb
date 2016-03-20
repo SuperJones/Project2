68e60c2f61475285dbe147dce914c806fc8c7bc1
Anissa Jones <anissajones10@gmail.com>
2016-03-20 00:21:13 -0400

    Adding actions.

---
+++ steps_battle/app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class ArtistsController < ApplicationController
+
+  def index
+      @users = User.all
+  end
+
+  def new
+      @user = User.new
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
