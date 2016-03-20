83c5c94419c986223806c1c6b4d2f51fb6844a6b
Anissa Jones <anissajones10@gmail.com>
2016-03-20 12:01:08 -0400

    committing before trying a feature.

--- steps_battle/app/controllers/users_controller.rb
+++ steps_battle/app/controllers/users_controller.rb
@@ -8,12 +8,13 @@ class UsersController < ApplicationController
       @user = User.new
   end
 
-  def show
-
+  def create
+      @user = User.create!(user_params)
+      redirect_to @user
   end
 
-  def create
-
+  def show
+      @user = User.find(params[:id])
   end
 
   def edit
