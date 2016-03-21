c539df3616f0f5dc84cb3d6d6a142d0f9dcab114
Anissa Jones <anissajones10@gmail.com>
2016-03-21 13:48:23 -0400

    edit function was running into error. Needed to add another argument to update action. Also redirected page to user_path.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,25 +8,29 @@ class UsersController < ApplicationController
       @user = User.new
   end
 
+  def show
+      @user = User.find(params[:id])
+  end
+
   def create
       @user = User.create!(user_params)
       redirect_to users_path
   end
 
-  def show
+  def edit
       @user = User.find(params[:id])
   end
 
-  def edit
-
-  end
-
   def update
-
+      @user = User.find(params[:id])
+      @user = User.update(@user, user_params)
+      redirect_to user_path
   end
 
   def destroy
-
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect_to users_path
   end
 
   private
