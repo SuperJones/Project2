ca2992d2393d178e18b760067c06caec987e9f85
Anissa Jones <anissajones10@gmail.com>
2016-03-22 17:21:24 -0400

    Adds sign up equals sign in and users can sign out.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,26 +8,30 @@ class UsersController < ApplicationController
       @user = User.new
   end
 
+  def create
+      @user = User.create!(user_params)
+      session[:user_id] = @user.id
+      redirect_to users_path(@user)
+  end
+
   def show
       @user = User.find(params[:id])
   end
 
-  def create
-      @user = User.create!(user_params)
-      redirect_to users_path
-  end
-
   def edit
       @user = User.find(params[:id])
+       redirect_to root_url unless @current_user == @user
   end
 
   def update
       @user = User.find(params[:id])
-      @user = User.update(@user, user_params)
-      redirect_to user_path
+      redirect_to root_url unless @current_user == @user
+      @user.update(user_params)
+      redirect_to user_path(@user)
   end
 
   def destroy
+      redirect_to root_path unless @current_user
       @user = User.find(params[:id])
       @user.destroy
       redirect_to users_path
