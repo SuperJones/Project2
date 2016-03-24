92b79809a8508490ee95c2a1a26e42c163ab747c
Anissa Jones <anissajones10@gmail.com>
2016-03-23 20:22:44 -0400

    seed creating error, because user already has inventories and resources is creating another inventory.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,37 @@
 #   cities = City.create([{ name: 'Chicago' } { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
+Inventory.destroy_all
 Weapon.destroy_all
 User.destroy_all
-Inventory.destroy_all
-
-basicsword = Weapon.create({weapon_kind: "sword", weapon_name: "Wooden Sword", hp: 10})
-basicstaff = Weapon.create({weapon_kind: "staff", weapon_name: "Simple Wooden Staff", hp: 10})
-basicarrow = Weapon.create({weapon_kind: "arrow", weapon_name: "Bow and No Tip Arrow", hp: 10})
-
-maria = User.create!({username: "mgutierrez0",steps: 314, photo_url: "thisismaria.png", email: "mariagutierrez@lollipants.com"})
-mike = User.create!({username: "mcoleman1",steps: 218, photo_url: "thisismike.png", email: "mikecoleman@handsompants.com"})
-rick = User.create!({username: "rwatson2",steps: 971, photo_url: "thisisrick.png", email: "rickwatson2@candypants.com"})
-sasha = User.create!({username: "spalmer3",steps: 805, photo_url: "thisissasha.png", email: "sashfierce@sassypants.com"})
-pam = User.create!({username: "pgarza4",steps: 259, photo_url: "thisispam.png", email: "pamgarza@simplepants.com"})
 
 
-Inventory.create(user: maria, weapon: basicsword)
-Inventory.create(user: mike, weapon: basicarrow)
-Inventory.create(user: rick, weapon: basicstaff)
+basicsword = Weapon.create!({ weapon_kind: "sword", weapon_name: "Wooden Sword", hp: 10 })
+basicstaff = Weapon.create!({ weapon_kind: "staff", weapon_name: "Simple Wooden Staff", hp: 10 })
+basicarrow = Weapon.create!({ weapon_kind: "arrow", weapon_name: "Bow and No Tip Arrow", hp: 10 })
+leveltwosword = Weapon.create!({weapon_kind: "sword", weapon_name: "Metal Sword", hp: 50 })
+leveltwostaff = Weapon.create!({weapon_kind: "staff", weapon_name: "Staff with Magic Orb on Top", hp: 50 })
 
-firstarrow = Weapon.create({weapon_kind: "sword", weapon_name: "Metal Sword", hp: 50})
-sasha.inventories.create(weapon: firstarrow)
 
-secondstaff = Weapon.create({weapon_kind: "staff", weapon_name: "Staff with Magic Orb on Top", hp: 50})
-pam.inventories.create(weapon: secondstaff)
+maria = User.create!({ username: "mgutierrez0",steps: 314, photo_url: "thisismaria.png", email: "mariagutierrez@lollipants.com" })
+mike = User.create!({ username: "mcoleman1",steps: 218, photo_url: "thisismike.png", email: "mikecoleman@handsompants.com" })
+rick = User.create!({ username: "rwatson2",steps: 971, photo_url: "thisisrick.png", email: "rickwatson2@candypants.com" })
+sasha = User.create!({ username: "spalmer3",steps: 805, photo_url: "thisissasha.png", email: "sashfierce@sassypants.com" })
+pam = User.create!({ username: "pgarza4",steps: 259, photo_url: "thisispam.png", email: "pamgarza@simplepants.com" })
+
+
+i = maria.inventories.first
+i.weapon = basicsword
+i.save
+i = mike.inventories.first
+i.weapon = basicstaff
+i.save
+i = rick.inventories.first
+i.weapon = basicarrow
+i.save
+i = sasha.inventories.first
+i.weapon = leveltwosword
+i.save
+i = pam.inventories.first
+i.weapon = leveltwostaff
+i.save
