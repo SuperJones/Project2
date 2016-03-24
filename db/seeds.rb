fb63ac3af02dd4808297070242966232ed11bbb8
Anissa Jones <anissajones10@gmail.com>
2016-03-23 22:43:59 -0400

    added seed pictures.

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,11 +18,11 @@ leveltwosword = Weapon.create!({weapon_kind: "sword", weapon_name: "Metal Sword"
 leveltwostaff = Weapon.create!({weapon_kind: "staff", weapon_name: "Staff with Magic Orb on Top", hp: 50 })
 
 
-maria = User.create!({ username: "mgutierrez0",steps: 314, photo_url: "thisismaria.png", email: "mariagutierrez@lollipants.com" })
-mike = User.create!({ username: "mcoleman1",steps: 218, photo_url: "thisismike.png", email: "mikecoleman@handsompants.com" })
-rick = User.create!({ username: "rwatson2",steps: 971, photo_url: "thisisrick.png", email: "rickwatson2@candypants.com" })
-sasha = User.create!({ username: "spalmer3",steps: 805, photo_url: "thisissasha.png", email: "sashfierce@sassypants.com" })
-pam = User.create!({ username: "pgarza4",steps: 259, photo_url: "thisispam.png", email: "pamgarza@simplepants.com" })
+maria = User.create!({ username: "mgutierrez0",steps: 314, photo_url: "http://i.imgur.com/1Keegdm.jpg", email: "mariagutierrez@lollipants.com" })
+mike = User.create!({ username: "mcoleman1",steps: 218, photo_url: "http://i.imgur.com/ewvc3Vv.jpg", email: "mikecoleman@handsompants.com" })
+rick = User.create!({ username: "rwatson2",steps: 971, photo_url: "http://i.imgur.com/t7Bs6Kz.jpg", email: "rickwatson2@candypants.com" })
+sasha = User.create!({ username: "spalmer3",steps: 805, photo_url: "http://i.imgur.com/3VfbkcJ.jpg", email: "sashfierce@sassypants.com" })
+pam = User.create!({ username: "pgarza4",steps: 259, photo_url: "http://i.imgur.com/Cl5LAuE.jpg", email: "pamgarza@simplepants.com" })
 
 
 i = maria.inventories.first
