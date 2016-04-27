a148ca215efda1586f91d0011ce36de691f45756
Anissa Jones <anissajones10@gmail.com>
2016-04-27 10:48:40 -0400

    Adds photo_url to edit/update options.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,10 +8,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :steps)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :steps, :photo_url)
   end
 
   def account_update_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password, :steps)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password, :steps, :photo_url)
   end
 end
