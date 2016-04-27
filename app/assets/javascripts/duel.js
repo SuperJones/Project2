8e5de0fa2d8fb653b2806c4130f70c535958becf
Anissa Jones <anissajones10@gmail.com>
2016-04-27 16:37:15 -0400

    Adds text to middle info portion and grabes hp info.

--- app/assets/javascripts/duel.js
+++ app/assets/javascripts/duel.js
@@ -1,17 +1,54 @@
 $(document).ready(function(){
 
-oppUsername = $("#oppUsername").html();
-currUsername = $("#currUsername").html();
+// Get usernames
+var oppUsername = $("#oppUsername").html();
+var currUsername = $("#currUsername").html();
 
-$('#oppweapon').addClass('active');
-$('#oppweapon').addClass('animated infinite pulse');
+//get weapon
+var oppweapon = $('#oppweapon');
+var currweapon = $('#currweapon');
 
+//Add Active class
+function toggleActive(){
+    oppweapon.toggleClass('active');
+    currweapon.toggleClass('active');
+}
 
-textParagraph = $('.middleInfo .fightStatus');
-textParagraph.html(oppUsername + " goes first");
+//Add Animation for active
+function isAnimated(){
+  if(oppweapon.hasClass("active")){
+    oppweapon.addClass('animated infinite pulse');
+  }else{
+    oppweapon.removeClass('animated infinite pulse');
+  }
 
-// TODO: Add another p with updated info
-// textParagraph.html(oppUsername + " goes first");
+  if(currweapon.hasClass("active")){
+    currweapon.addClass('animated infinite pulse');
+  }else{
+    currweapon.removeClass('animated infinite pulse');
+  }
+}
 
+// Initiat opponent with active and Add animation to active user
+oppweapon.addClass('active');
+isAnimated();
+
+// say who is the first to attack
+$('#middleInfo .fightStatus').html(oppUsername + " goes first");
+
+//opponent attacks current_user
+$('#middleInfo p:last').after("<p></p>");
+$('#middleInfo p:last').append(oppUsername + " ATTACKS " + currUsername);
+
+// TODO: update current_user HP and say how many points they lost
+$('#middleInfo p:last').after("<p></p>");
+$('#middleInfo p:last').append(currUsername + " loses points");
+
+function decrement(){
+  var currhp = $("#currhp").html();
+  alert("The current users hp equals " + currhp);
+}
+
+decrement();
 
 });
