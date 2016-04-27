ec02d15d5642011b1b88ef50b792472579a8f125
Anissa Jones <anissajones10@gmail.com>
2016-04-27 14:26:10 -0400

    Adds classes and ids to give jquery access. Adds animation to photo and displays first user.

--- app/assets/javascripts/duel.js
+++ app/assets/javascripts/duel.js
@@ -1,1 +1,17 @@
+$(document).ready(function(){
 
+oppUsername = $("#oppUsername").html();
+currUsername = $("#currUsername").html();
+
+$('#oppweapon').addClass('active');
+$('#oppweapon').addClass('animated infinite pulse');
+
+
+textParagraph = $('.middleInfo .fightStatus');
+textParagraph.html(oppUsername + " goes first");
+
+// TODO: Add another p with updated info
+// textParagraph.html(oppUsername + " goes first");
+
+
+});
